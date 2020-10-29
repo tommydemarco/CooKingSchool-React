ff7924a60181f2950790810486a828218ebd7cb9
tommydemarco <tommaso.demarco95@gmail.com>
2020-10-29 13:15:04 +0000

    Sidebar layout and styling added

--- src/components/layouts/TheSideNav.js
+++ src/components/layouts/TheSideNav.js
@@ -3,7 +3,44 @@ import React from 'react';
 function TheSideNav() {
     return (
         <nav className="sidebar">
-            Navigation
+            <ul class="side-nav">
+                <li className="side-nav__item side-nav__item--active">
+                    <a href="/" className="side-nav__link">
+                        <svg className="side-nav__icon">
+                            <use xlinkHref="/assets/img/sprite.svg#icon-search"></use>
+                        </svg>
+                        <span>Navigation</span>
+                    </a>
+                </li>
+                <li className="side-nav__item">
+                    <a href="/" className="side-nav__link">
+                        <svg className="side-nav__icon">
+                            <use xlinkHref="/assets/img/sprite.svg#icon-search"></use>
+                        </svg>
+                        <span>Navigation</span>
+                    </a>
+                </li>
+                <li className="side-nav__item">
+                    <a href="/" className="side-nav__link">
+                        <svg className="side-nav__icon">
+                            <use xlinkHref="/assets/img/sprite.svg#icon-search"></use>
+                        </svg>
+                        <span>Navigation</span>
+                    </a>
+                </li>
+                <li className="side-nav__item">
+                    <a href="/" className="side-nav__link">
+                        <svg className="side-nav__icon">
+                            <use xlinkHref="/assets/img/sprite.svg#icon-search"></use>
+                        </svg>
+                        <span>Navigation</span>
+                    </a>
+                </li>
+            </ul>
+
+            <div className="legal">
+                You are logged in as Tommy
+            </div>
         </nav>
     );
 }
